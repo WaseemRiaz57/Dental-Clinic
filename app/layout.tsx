5ac63aecd8145f7131a3f4a0fe56680a0cc38466
Waseem Riaz <136987763+WaseemRiaz57@users.noreply.github.com>
2026-03-12 07:18:17 +0500

    dark mode mobile responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="light" suppressHydrationWarning>
       <head>
         <link
           rel="stylesheet"
